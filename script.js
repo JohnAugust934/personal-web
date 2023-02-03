05fbacff18141212270374d4a6ab4637871f8de3
João Augusto <49285001+JohnAugust934@users.noreply.github.com>
2023-02-03 15:15:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const header = document.querySelector("header");
+const main = document.querySelector("main");
+const footer = document.querySelector("footer");
+
+header.addEventListener("mouseenter", () => {
+    header.style.backgroundColor = "dodgerblue";
+});
