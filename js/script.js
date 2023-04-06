351f9e3ea5078106a974ec3fad21a2b8264e76e7
João Augusto <49285001+JohnAugust934@users.noreply.github.com>
2023-04-06 13:22:07 -0300

    email em branco

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const header = document.querySelector("header");
-const main = document.querySelector("main");
-const footer = document.querySelector("footer");
-
-header.addEventListener("mouseenter", () => {
-    header.style.backgroundColor = "dodgerblue";
-});
